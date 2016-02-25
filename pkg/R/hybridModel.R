3300e80186e042f20eb1c5210e673328eea93a63
David Shaub <dashaub@users.noreply.github.com>
2016-02-25 09:44:57 -0700

    Check xreg nrows against length of y

--- pkg/R/hybridModel.R
+++ pkg/R/hybridModel.R
@@ -28,63 +28,68 @@ hybridModel <- function(y, models = "aten",
                         weights = c("equal", "insample.errors","cv.errors"),
                         errorMethod = c("rmse", "mae", "mase"),
                         parallel = TRUE, num.cores = 2){
-   # Weights could be set to equal (the default), based on in-sample errors, or based on cv errors
-   # In-sample errors are methodologically questionable but easy to implement. CV errors are probably
-   # methodologicaly sound, but more difficult and computationally expensive to implement.
-   
-   # errorMethod will determine which type of errors to use for weights. Some choices from accuracy()
-   # are not appropriate. If weights = "equal", this would be ignored.
-   
-   # The dependent variable must be numeric and not a matrix/dataframe
-   if(!is.numeric(y) || !is.null(dim(y))){
+  # Weights could be set to equal (the default), based on in-sample errors, or based on cv errors
+  # In-sample errors are methodologically questionable but easy to implement. CV errors are probably
+  # methodologicaly sound, but more difficult and computationally expensive to implement.
+  
+  # errorMethod will determine which type of errors to use for weights. Some choices from accuracy()
+  # are not appropriate. If weights = "equal", this would be ignored.
+  
+  # The dependent variable must be numeric and not a matrix/dataframe
+  if(!is.numeric(y) || !is.null(dim(y))){
     stop("The time series must be numeric and may not be a matrix or dataframe object")
-   }
-   y <- as.ts(y)
-   
-   # Match arguments to ensure validity
-   weights <- match.arg(weights)
-   errorMethod <- match.arg(errorMethod)
-   
-   # Match the specified models
-   expandedModels <- unique(tolower(unlist(strsplit(models, split = ""))))
-   if(length(expandedModels) > 4){
+  }
+  y <- as.ts(y)
+  
+  # Match arguments to ensure validity
+  weights <- match.arg(weights)
+  errorMethod <- match.arg(errorMethod)
+  
+  # Match the specified models
+  expandedModels <- unique(tolower(unlist(strsplit(models, split = ""))))
+  if(length(expandedModels) > 4){
     stop("Invalid models specified.")
-   }
-   # All characters must be valid
-   if(!(all(expandedModels %in% c("a", "t", "e", "n")))){
+  }
+  # All characters must be valid
+  if(!(all(expandedModels %in% c("a", "t", "e", "n")))){
     stop("Invalid models specified.")
-   }
-   
-   
-   modelResults <- list()
-   
-   # We would allow for these models to run in parallel at the model level rather than within the model
-   # since this has better performance. As an enhancement, users with >4 cores could benefit by running
-   # parallelism both within and between models, based on the number of available cores.
-   
-   # auto.arima(), additional arguments to be implemented
-   if(is.element("a", expandedModels)){
-      modelResults$auto.arima <- auto.arima(y, xreg = xreg)
-   }
-   # tbats(), additional arguments to be implemented
-   if(is.element("t", expandedModels)){
-      modelResults$tbats <- tbats(y)
-   }
-   # ets(), additional arguments to be implemented
-   if(is.element("e", expandedModels)){
-      modelResults$ets <- ets(y)
-   }
-   # nnetar(), additional arguments to be implemented
-   if(is.element("n", expandedModels)){
-      modelResults$nnetar <- nnetar(y)
-   }
-   
-   # Set the model weights
-   includedModels <- names(modelResults)
-   modelResults$weights <- rep(1 / length(expandedModels), length(expandedModels))
-   names(modelResults$weights) <- includedModels
-   
-   class(modelResults) <- "hybridModel"
-   modelResults$frequency <- frequency(y)
-   return(modelResults)
+  }
+  
+  # xreg must have same number of observations as the timeseries
+  if(!is.null(xreg) & nrow(xreg) != length(y)){
+    stop("The supplied xreg must have the same number of rows as the timeseries")
+  }
+  
+  
+  modelResults <- list()
+  
+  # We would allow for these models to run in parallel at the model level rather than within the model
+  # since this has better performance. As an enhancement, users with >4 cores could benefit by running
+  # parallelism both within and between models, based on the number of available cores.
+  
+  # auto.arima(), additional arguments to be implemented
+  if(is.element("a", expandedModels)){
+    modelResults$auto.arima <- auto.arima(y, xreg = xreg)
+  }
+  # tbats(), additional arguments to be implemented
+  if(is.element("t", expandedModels)){
+    modelResults$tbats <- tbats(y)
+  }
+  # ets(), additional arguments to be implemented
+  if(is.element("e", expandedModels)){
+    modelResults$ets <- ets(y)
+  }
+  # nnetar(), additional arguments to be implemented
+  if(is.element("n", expandedModels)){
+    modelResults$nnetar <- nnetar(y)
+  }
+  
+  # Set the model weights
+  includedModels <- names(modelResults)
+  modelResults$weights <- rep(1 / length(expandedModels), length(expandedModels))
+  names(modelResults$weights) <- includedModels
+  
+  class(modelResults) <- "hybridModel"
+  modelResults$frequency <- frequency(y)
+  return(modelResults)
 }
