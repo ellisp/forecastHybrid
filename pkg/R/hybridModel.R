5191d1ca0ae5d0db4cfa66418f32194b7ecdd2b9
David Shaub <dashaub@users.noreply.github.com>
2016-03-20 21:30:21 -0600

    stlm also not supported by accuracy()

--- pkg/R/hybridModel.R
+++ pkg/R/hybridModel.R
@@ -36,8 +36,8 @@
 #' @examples
 #' mod1 <- hybridModel(AirPassengers)
 #' plot(forecast(mod1))
-#' mod2 <- hybridModel(AirPassengers, models = "aest", weights = "insample.errors", errorMethod = "MASE")
-#' mod3 <- hybridModel(AirPassengers, models = "aen", a.args = list(max.p = 7, mas.q = 7, approximation = FALSE))
+#' mod2 <- hybridModel(AirPassengers, models = "aet", weights = "insample.errors", errorMethod = "MASE")
+#' mod3 <- hybridModel(AirPassengers, models = "aens", a.args = list(max.p = 7, mas.q = 7, approximation = FALSE))
 #'
 hybridModel <- function(y, models = "aenst",
                         lambda = NULL,
