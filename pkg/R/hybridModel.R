e106ad8de51b2bb56a2ee5f6f9dd1dd4eb1c4766
David Shaub <dashaub@users.noreply.github.com>
2016-03-20 21:36:03 -0600

    fix typo

--- pkg/R/hybridModel.R
+++ pkg/R/hybridModel.R
@@ -37,7 +37,7 @@
 #' mod1 <- hybridModel(AirPassengers)
 #' plot(forecast(mod1))
 #' mod2 <- hybridModel(AirPassengers, models = "aet", weights = "insample.errors", errorMethod = "MASE")
-#' mod3 <- hybridModel(AirPassengers, models = "aens", a.args = list(max.p = 7, mas.q = 7, approximation = FALSE))
+#' mod3 <- hybridModel(AirPassengers, models = "aens", a.args = list(max.p = 7, max.q = 7, approximation = FALSE))
 #'
 hybridModel <- function(y, models = "aenst",
                         lambda = NULL,
