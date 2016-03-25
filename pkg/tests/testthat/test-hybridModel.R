ce221467c61e760a636d7f2ea5e9872da73ee251
David Shaub <dashaub@users.noreply.github.com>
2016-03-25 12:12:06 -0600

    update unit tests

--- pkg/tests/testthat/test-hybridModel.R
+++ pkg/tests/testthat/test-hybridModel.R
@@ -6,16 +6,22 @@ if(require(fpp) & require(forecast) & require(testthat)){
     expect_error(hybridModel(y = 1:10, models = 5))
     expect_error(hybridModel(y = matrix(1:10, nrow = 5, ncol = 2), models = 5))
     # Test for invalid mistmatch length of y and xreg in a.args/s.args later?
-    expect_warning(expect_that(hybridModel(y = wineind, a.args = list(xreg = rnorm(length(wineind) - 1))), throws_error()))
-    expect_that(hybridModel(y = rnorm(100), models = "ae", a.args = list(xreg = matrix(runif(100), nrow = 100))), not(throws_error()))
+    expect_error(hybridModel(y = wineind, a.args = list(xreg = rnorm(length(wineind) - 1))))
     expect_error(hybridModel(y = "hello world"))
     expect_error(hybridModel())
     expect_error(hybridModel(y = numeric()))
     expect_error(hybridModel(y = "aten"))
+    expect_error(hybridModel(wineind, models = "a"))
     expect_error(hybridModel(wineind, num.cores = -1L))
     expect_error(hybridModel(wineind, num.cores = 3.3))
     expect_error(hybridModel(wineind, num.cores = "a"))
     expect_error(hybridModel(wineind, models = ""))
+    
+  })
+  test_that("Testing valid inputs", {
+    set.seed(54321)
+    expect_that(hybridModel(y = rnorm(100), models = "ae", a.args = list(xreg = matrix(runif(100), nrow = 100))), not(throws_error()))
+    expect_warning(hybridModel(y = rnorm(10), models = "en", a.args = list(lambda = 0.5)))
     expect_that(hybridModel(wineind, models = "atens"), not(throws_error()))
   })
   test_that("Testing model matching", {
