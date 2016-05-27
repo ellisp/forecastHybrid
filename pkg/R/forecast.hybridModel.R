3c4842d905d78a13afec3ce301ad78e5fef8f0ff
David Shaub <dashaub@users.noreply.github.com>
2016-05-27 06:48:58 -0600

    fix warning for nrow(xreg) != h

--- pkg/R/forecast.hybridModel.R
+++ pkg/R/forecast.hybridModel.R
@@ -45,7 +45,7 @@ forecast.hybridModel <- function(object, h = ifelse(object$frequency > 1, 2 * ob
     if(!is.numeric(xreg)){
       stop("The supplied xreg must be numeric.")
     }
-    if(nrow(xreg) != h && !missing(h)){
+    if((nrow(xreg) != h) && (missing(h))){
       warning("The number of rows in xreg should match h. Setting h to nrow(xreg).")
       h <- nrow(xreg)
     }
