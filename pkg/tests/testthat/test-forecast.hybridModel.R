ce36e7b2fb8fe47749ea6f212652b4f750bad9b2
David Shaub <dashaub@users.noreply.github.com>
2016-05-27 11:23:34 -0600

    formatting and catch warning for nrow(xreg) != h

--- pkg/tests/testthat/test-forecast.hybridModel.R
+++ pkg/tests/testthat/test-forecast.hybridModel.R
@@ -1,15 +1,18 @@
 # Unit tests on the hybridModel function
-if(require(forecast) & require(testthat)){
+if(require(forecast) &  require(testthat)){
   context("Testing input for forecast.hybridModel()")
   test_that("Testing invalid inputs", {
     hModel <- hybridModel(woolyrnq)
-    expect_error(forecast(object = 1))
-    expect_error(forecast(object = hModel, h = -1L))
-    expect_error(forecast(object = hModel, h = "a"))
-    expect_error(forecast(object = hModel, h = 3.2))
-    expect_error(forecast(object = hModel, h = 5, xreg = matrix(1:5, nrow = 5, ncol = 2)))
-    expect_error(forecast(object = hModel, h = 5, xreg = matrix("a", nrow = 5, ncol = 2)))
-    expect_error(forecast(object = hModel, h = 5, xreg = 1:12))
+    expect_error(forecast.hybridModl(object = 1))
+    expect_error(forecast.hybridModl(object = hModel, h = -1L))
+    expect_error(forecast.hybridModl(object = hModel, h = "a"))
+    expect_error(forecast.hybridModl(object = hModel, h = 3.2))
+    expect_error(forecast.hybridModl(object = hModel, h = 5,
+                                     xreg = matrix(1:5, nrow = 5, ncol = 2)))
+    expect_error(forecast.hybridModl(object = hModel, h = 5,
+                                     xreg = matrix("a", nrow = 5, ncol = 2)))
+    expect_error(forecast.hybridModl(object = hModel, h = 5,
+                                     xreg = 1:12))
   })
   test_that("Testing forecasts with xreg", {
     mm <- matrix(runif(length(wineind)), nrow = length(wineind))
@@ -21,7 +24,7 @@ if(require(forecast) & require(testthat)){
     expect_true(length(forecast(aa, xreg = matrix(rnorm(24), nrow = 24))$mean) == 24L)
     expect_true(class(forecast(aa, xreg = matrix(rnorm(24), nrow = 24))) == "forecast")
     expect_true(all(forecast(aa, xreg = mm,  h = nrow(mm), level = 0.9)$upper == forecast(aa, xreg = mm,  h = nrow(mm), level = 90)$upper))
-    expect_error(forecast(aa, xreg = mm, level = 110))
+    expect_warning(expect_error(forecast(aa, xreg = mm, level = 110)))
     expect_true(ncol(forecast(aa, xreg = mm, h = nrow(mm), fan = TRUE)$upper) == 17)
   })
 }
