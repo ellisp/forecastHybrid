d5cb9d3a0da782956c51266577b1cbba3de654cb
David Shaub <dashaub@users.noreply.github.com>
2016-06-13 12:48:43 -0600

    more robust xreg and h checks

--- pkg/R/forecast.hybridModel.R
+++ pkg/R/forecast.hybridModel.R
@@ -36,12 +36,17 @@ forecast.hybridModel <- function(object,
                                  xreg = NULL,
                                  level = c(80, 95),
                                  fan = FALSE, ...){
-
+  
   # Check inputs
   if(!is.hybridModel(object)){
     stop("The object must be constructed from hybridModel().")
   }
-
+  
+  # apply nrow(xreg) to h if h isn't provided and xreg is
+  if(missing(h) && !missing(xreg)){
+    h <- nrow(xreg)
+  }
+  
   # xreg should be a matrix and have same number of observations as the horizon
   if(!is.null(xreg)){
     if(!is.matrix(xreg) && !is.data.frame(xreg)){
@@ -56,15 +61,15 @@ forecast.hybridModel <- function(object,
       h <- nrow(xreg)
     }
   }
-
+  
   # Check the forecast horizon
   if(!is.numeric(h)){
     stop("The forecast horizon h must be a positive integer.")
   }
-  if(as.logical((h %% 1L)) || h <= 0L){
+  if(as.logical((h %% 1L == 0L)) || h <= 0L){
     stop("The forecast horizon h must be a positive integer.")
   }
-
+  
   # Allow for fan prediction intervals
   if(fan){
     level <- seq(51, 99, by = 3)
@@ -75,8 +80,8 @@ forecast.hybridModel <- function(object,
       stop("Confidence limit out of range")
     }
   }
-
-
+  
+  
   # This code is pretty ugly, There is probably a better way of doing this.
   forecastWeights <- object$weights
   weightsMatrix <- matrix(rep(forecastWeights, times = h), nrow = h, byrow = TRUE)
@@ -104,7 +109,7 @@ forecast.hybridModel <- function(object,
     forecasts$tbats <- forecast(object$tbats, h = h, level = level)
     forecasts$pointForecasts[, "tbats"] <- forecasts$tbats$mean
   }
-
+  
   # Apply the weights to the individual forecasts and create the final point forecast
   finalForecast <- rowSums(forecasts$pointForecast * weightsMatrix)
   # Conver the final forecast into a ts object
@@ -112,14 +117,14 @@ forecast.hybridModel <- function(object,
                       start = start(forecasts[[object$models[1]]]$mean),
                       end = end(forecasts[[object$models[1]]]$mean),
                       frequency = object$frequency)
-
+  
   # Apply the weights to construct the fitted values
   fits <- sapply(includedModels, FUN = function(x) fitted(object[[x]]))
   fitsWeightsMatrix <- matrix(rep(forecastWeights, times = nrow(fits)),
                               nrow = nrow(fits), byrow = TRUE)
   fits <- rowSums(fits * fitsWeightsMatrix)
   resid <- object$x - fits
-
+  
   # Construct the prediction intervals
   nint <- length(level)
   upper <- lower <- matrix(NA, ncol = nint, nrow = length(finalForecast))
@@ -146,17 +151,17 @@ forecast.hybridModel <- function(object,
   colnames(lower) <- colnames(upper) <- paste0(level, "%")
   forecasts$lower <- lower
   forecasts$upper <- upper
-
+  
   # Build the mean forecast as a ts object
   tsp.x <- tsp(object$x)
-#   if (!is.null(tsp.x)){
-#     start.f <- tsp(object$x)[2] + 1/object$frequency
-#   } else{
-#     start.f <- length(object$x) + 1
-#   }
-#   stop.f <- start.f + h / object$frequency
+  #   if (!is.null(tsp.x)){
+  #     start.f <- tsp(object$x)[2] + 1/object$frequency
+  #   } else{
+  #     start.f <- length(object$x) + 1
+  #   }
+  #   stop.f <- start.f + h / object$frequency
   forecasts$mean <- finalForecast
-
+  
   # Add the fitted and residuals values
   if(is.ts(object$x)){
     fits <- ts(fits)
@@ -165,7 +170,7 @@ forecast.hybridModel <- function(object,
   }
   forecasts$fitted <- fits
   forecasts$residuals <- resid
-
+  
   # Build a forecast object
   forecasts$x <- forecasts[[object$models[1]]]$x
   forecasts$method <- paste0(object$models, " with weight ", object$weights)
