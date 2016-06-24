93230ffad4c9a3c3dce26c959988047e1ac6c294
David Shaub <dashaub@users.noreply.github.com>
2016-06-24 16:23:39 -0600

    Create test-cvts.R

---
+++ pkg/tests/testthat/test-cvts.R
@@ -0,0 +1,9 @@
+# Unit tests on the cvts function
+if(require(forecast) &  require(testthat)){
+  context("Testing input for cvts()")
+  test_that("Testing invalid inputs", {
+    expect_error(cvts("invalid"))
+    expect_error(cvts(AirPassengers, useHorizon = 0L))
+    expect_error(cvts(AirPassengers, windowSize = 3.2))
+  })
+}
