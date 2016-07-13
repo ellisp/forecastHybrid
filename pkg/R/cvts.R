3ccefcc1db6e5f4598d7461141543bc45706e058
David Shaub <dashaub@users.noreply.github.com>
2016-07-13 14:52:15 -0600

    development on nonrolling cv

--- pkg/R/cvts.R
+++ pkg/R/cvts.R
@@ -37,32 +37,52 @@ cvts <- function(x, FUN = NULL, FCFUN = NULL,
      stop("The time series must be longer than windowSize + 2 * maxHorizon.")
    }
    
-   # Adapted from Rob Hyndman's approach
-   # http://robjhyndman.com/hyndsight/tscvexample/
-   k <- windowSize # minimum data length for fitting a model
-   n <- length(x)
-   forecasts <- mae <- mase <- matrix(NA, n - k, maxHorizon)
-   st <- tsp(x)[1] + (k - 2) / maxHorizon
-   fits <- vector("list", n - k)
-   for(i in 1:(n - k))
-   {
-      print(paste(i, "of", n - k))
-      xshort <- window(x, end = st + i/ maxHorizon)
-      xnext <- window(x, start = st + (i + 1) / maxHorizon, end = st + (i + maxHorizon) / maxHorizon)
-      fit <- auto.arima(xshort)
-      fcast <- forecast(fit, h = maxHorizon)
-      
-      forecasts[i, 1:length(xnext)] <- fcast$mean
-#       rmse[i,1:length(xnext)] <- abs(fcast[['mean']] - xnext)
-#       mae[i,1:length(xnext)] <- abs(fcast[['mean']] - xnext)
-#       mase[i,1:length(xnext)] <- abs(fcast[['mean']] - xnext)
-      #temporary, for debugging
-      fits[[i]] <- fit
+   if(!rolling){
+      # Number of rows will be determined by the series length, windowSize and maxHorizon
+      # Number of columns is the maxHorizon
+      results <- matrix(NA,
+                        nrow = as.integer((length(x) - windowSize) / maxHorizon),
+                        ncol = maxHorizon)
+      i <- 1
+      while(windowSize + maxHorizon <= length(x)){
+         x <- x[1:windowSize]
+         mod <- auto.arima(x)
+         fc <- forecast(mod, h = maxHorizon)$mean
+         results[i, ] <- fc
+         windowSize <- windowSize + maxHorizon
+         i <- i + 1
+      }
    }
-   # this will be replaced with the fitted values
-#    forecasted <- data
-#    return(accuracy(f = forecasted, x = data))
-   result <- list(forecasts, fits = fits)
-   class(result) <- "cv"
-   return(result)
+   
+   
+#    # Adapted from Rob Hyndman's approach
+#    # http://robjhyndman.com/hyndsight/tscvexample/
+#    k <- windowSize # minimum data length for fitting a model
+#    n <- length(x)
+#    forecasts <- mae <- mase <- matrix(NA, n - k, maxHorizon)
+#    st <- tsp(x)[1] + (k - 2) / maxHorizon
+#    fits <- vector("list", n - k)
+#    for(i in 1:(n - k))
+#    {
+#       if(verbose){
+#          print(paste(i, "of", n - k))
+#       }
+#       xshort <- window(x, end = st + i/ maxHorizon)
+#       xnext <- window(x, start = st + (i + 1) / maxHorizon, end = st + (i + maxHorizon) / maxHorizon)
+#       fit <- auto.arima(xshort)
+#       fcast <- forecast(fit, h = maxHorizon)
+#       
+#       forecasts[i, 1:length(xnext)] <- fcast$mean
+# #       rmse[i,1:length(xnext)] <- abs(fcast[['mean']] - xnext)
+# #       mae[i,1:length(xnext)] <- abs(fcast[['mean']] - xnext)
+# #       mase[i,1:length(xnext)] <- abs(fcast[['mean']] - xnext)
+#       #temporary, for debugging
+#       fits[[i]] <- fit
+#    }
+#    # this will be replaced with the fitted values
+# #    forecasted <- data
+# #    return(accuracy(f = forecasted, x = data))
+#    result <- list(forecasts, fits = fits)
+#    class(result) <- "cv"
+   return(results)
 }
