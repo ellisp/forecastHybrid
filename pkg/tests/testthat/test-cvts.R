166b94b9ce64fec81ef93a72c052c2cb5688eaa8
David Shaub <dashaub@users.noreply.github.com>
2016-07-14 11:15:59 -0600

    unit tests now use arbitrary FUN/FCFUN in cvts

--- pkg/tests/testthat/test-cvts.R
+++ pkg/tests/testthat/test-cvts.R
@@ -3,11 +3,12 @@ if(require(forecast) &  require(testthat)){
   context("Testing input for cvts()")
   test_that("Testing invalid inputs", {
     expect_error(cvts("invalid"))
-    expect_error(cvts(AirPassengers, useHorizon = 0L))
-    expect_error(cvts(AirPassengers, windowSize = 3.2))
-    expect_error(cvts(AirPassengers, windowSize = 130, maxHorizon = 12))
+    expect_error(cvts(AirPassengers, FUN = ets, FCFUN = forecast, useHorizon = 0L))
+    expect_error(cvts(AirPassengers, FUN = stlm, FCFUN = forecast, windowSize = 3.2))
+    expect_error(cvts(AirPassengers, FUN = ets, FCFUN = forecast, windowSize = 130, maxHorizon = 12))
   })
   test_that("Testing valid inputs", {
-     expect_error(cvts(AirPassengers, rolling = FALSE, windowSize = 60, maxHorizon = 12), NA)
+     expect_error(cvts(AirPassengers, FUN = auto.arima, FCFUN = forecast, rolling = FALSE, windowSize = 60, maxHorizon = 12), NA)
+     expect_error(cvts(AirPassengers, FUN = ets, FCFUN = forecast, rolling = FALSE, windowSize = 60, maxHorizon = 12), NA)
   })
 }
