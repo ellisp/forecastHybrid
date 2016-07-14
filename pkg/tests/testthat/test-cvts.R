92f21d21c452ccc875ae336e8ec9309773fab295
David Shaub <dashaub@users.noreply.github.com>
2016-07-14 14:22:37 -0600

    test rolling cv

--- pkg/tests/testthat/test-cvts.R
+++ pkg/tests/testthat/test-cvts.R
@@ -4,11 +4,11 @@ if(require(forecast) &  require(testthat)){
   test_that("Testing invalid inputs", {
     expect_error(cvts("invalid"))
     expect_error(cvts(AirPassengers, FUN = ets, FCFUN = forecast, useHorizon = 0L))
-    expect_error(cvts(AirPassengers, FUN = stlm, FCFUN = forecast, windowSize = 3.2))
+    expect_error(cvts(AirPassengers, FUN = ets, FCFUN = forecast, windowSize = 3.2))
     expect_error(cvts(AirPassengers, FUN = ets, FCFUN = forecast, windowSize = 130, maxHorizon = 12))
   })
   test_that("Testing valid inputs", {
-     expect_error(cvts(AirPassengers, FUN = auto.arima, FCFUN = forecast, rolling = FALSE, windowSize = 60, maxHorizon = 12), NA)
      expect_error(cvts(AirPassengers, FUN = ets, FCFUN = forecast, rolling = FALSE, windowSize = 60, maxHorizon = 12), NA)
+     expect_error(cvts(AirPassengers, FUN = ets, FCFUN = forecast, rolling = TRUE, windowSize = 60, maxHorizon = 12), NA)
   })
 }
