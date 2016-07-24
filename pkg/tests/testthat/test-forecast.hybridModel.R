f8bc748cd7705aef7feb428028079d43e74e3ee9
David Shaub <dashaub@users.noreply.github.com>
2016-07-24 08:37:15 -0600

    fix tests for forecast.hybridModel xreg behavior

--- pkg/tests/testthat/test-forecast.hybridModel.R
+++ pkg/tests/testthat/test-forecast.hybridModel.R
@@ -18,13 +18,15 @@ if(require(forecast) &  require(testthat)){
     mm <- matrix(runif(length(wineind)), nrow = length(wineind))
     expect_error(aa <- hybridModel(wineind, models = "aenst", a.args = list(xreg = mm), s.args = list(xreg = mm)))
     aa <- hybridModel(wineind, models = "aenst", a.args = list(xreg = mm), n.args = list (xreg = mm))
-    expect_warning(tmp <- forecast(aa, xreg = matrix(rnorm(20), nrow = 20)))
+    # If xreg is used and no h is provided, overwrite h
+    expect_error(tmp <- forecast(aa, xreg = matrix(rnorm(20), nrow = 20)), NA)
+    # If nrow(xreg) != h, issue a warning but set h <- nrow(xreg)
     expect_warning(forecast(aa, h = 10, xreg = matrix(rnorm(20), nrow = 20)))
     expect_error(forecast(aa, xreg = matrix(rnorm(24), nrow = 24)), NA)
     expect_true(length(forecast(aa, xreg = matrix(rnorm(24), nrow = 24))$mean) == 24L)
     expect_true(class(forecast(aa, xreg = matrix(rnorm(24), nrow = 24))) == "forecast")
     expect_true(all(forecast(aa, xreg = mm,  h = nrow(mm), level = 0.9)$upper == forecast(aa, xreg = mm,  h = nrow(mm), level = 90)$upper))
-    expect_warning(expect_error(forecast(aa, xreg = mm, level = 110)))
+    expect_error(forecast(aa, xreg = mm, level = 110))
     expect_true(ncol(forecast(aa, xreg = mm, h = nrow(mm), fan = TRUE)$upper) == 17)
   })
 }
