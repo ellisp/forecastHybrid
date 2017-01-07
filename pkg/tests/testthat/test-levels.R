83b09502e07c1ad03fc214ca0daf6f9d5592a913
David Shaub <dashaub@users.noreply.github.com>
2017-01-07 16:30:24 -0700

    faster levels test of forecast.hybridModel

--- pkg/tests/testthat/test-levels.R
+++ pkg/tests/testthat/test-levels.R
@@ -1,6 +1,8 @@
 
 test_that("forecast nnetar prediction intervals with different levels work", {
-   hm2 <- hybridModel(wineind, models = "anst", weights = "equal")
-   expect_error(forecast(hm2, h = 48), NA)
-   expect_error(forecast(hm2, h = 48, level = c(70, 80, 90, 95)), NA)
+   inputSeries <- ts(rnorm(9), f = 4)
+   hm2 <- hybridModel(inputSeries, models = "afn", weights = "equal")
+   # forecast with nnetar and PI = TRUE is very slow, so use few npaths
+   expect_error(forecast(hm2, h = 48, npaths = 10), NA)
+   expect_error(forecast(hm2, h = 48, level = c(70, 80, 90, 95), npaths = 10), NA)
 })
