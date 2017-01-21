72c905f2da253bd4afc2ee8e6a65baa3be37608a
Ganesh <gkrishnan.gt@gmail.com>
2017-01-21 14:06:52 -0800

    Improved robustness of tsCombine
    Can now combine non-sequential series as long as the frequency is the same

--- pkg/R/helper.R
+++ pkg/R/helper.R
@@ -12,16 +12,25 @@
 #' 
 #' @details Combine sequential time series objects into a single time series object. This might
 #' be useful, for example, when you want to combine the training and validation time series objects
-#' for plotting. The function assumes that the provided objects refer to a sequential time.
+#' for plotting. The function assumes that the provided objects have no overlap.
 #' For example, a valid argument set would have two time series with periods from Jan-Dec 2015
-#' and Jan-Dec 2016. An invalid set would be two time series with periods from Jan-Dec 2015
-#' and Feb-Dec 2016. No sanity checks are performed, so it is the caller's responsiblity to ensure
-#' that the time series are sequential
+#' and Jan-Dec 2016. An invalid set would be two time series t1 and t2 with periods from 
+#' Jan-Dec 2015 and Aug 2015-Dec 2016 respectively. In that case, there is overlap between 
+#' t1 and t2. The return value will depend on the order in which the arguments are provided. 
+#' If the function call is tsCombine(t1, t2), the overlapping portion of t1 and t2 
+#' (Aug-Dec 2015 in this example), would have values from t1 as long as they are not NA. 
+#' If the call is tsCombine(t2, t1), it will have values from t2 as long as they are not NA. 
 #' 
 #' @examples 
 #' tsCombine(window(AirPassengers, end = c(1951, 12)), window(AirPassengers, start = c(1952, 1)))
 tsCombine <- function(...) {
-   ts.union(..., dframe = TRUE) %>%
-      apply(1, function(x) x[min(which(!is.na(x)))]) %>%
-      ts(start = start(...), frequency = frequency(...))
+   combined_df <- ts.union(..., dframe = TRUE)
+   combined_ts <- ts.union(..., dframe = FALSE)
+   coalesced <- apply(combined_df, 1, function(x) {
+      if (all(is.na(x))) 
+         return(NA)
+      else return(x[min(which(!is.na(x)))])
+   })
+   ret <- ts(coalesced, start = start(combined_ts), frequency = frequency(combined_ts))
+   return(ret)
 }
