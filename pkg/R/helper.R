0301844c6fc26a1c5d440eb06095af62b4659224
ganesh_krishnan <ganesh.krishnan@instacart.com>
2017-01-21 19:49:26 -0800

    Created function to subset time series using indices

--- pkg/R/helper.R
+++ pkg/R/helper.R
@@ -25,13 +25,35 @@
 #' @examples 
 #' tsCombine(window(AirPassengers, end = c(1951, 12)), window(AirPassengers, start = c(1952, 1)))
 tsCombine <- function(...) {
-   combined_df <- ts.union(..., dframe = TRUE)
-   combined_ts <- ts.union(..., dframe = FALSE)
-   coalesced <- apply(combined_df, 1, function(x) {
-      if (all(is.na(x))) 
-         return(NA)
-      else return(x[min(which(!is.na(x)))])
-   })
-   ret <- ts(coalesced, start = start(combined_ts), frequency = frequency(combined_ts))
-   return(ret)
+  combined_df <- ts.union(..., dframe = TRUE)
+  combined_ts <- ts.union(..., dframe = FALSE)
+  coalesced <- apply(combined_df, 1, function(x) {
+    if (all(is.na(x))) 
+      return(NA)
+    else return(x[min(which(!is.na(x)))])
+  })
+  ret <- ts(coalesced, start = start(combined_ts), frequency = frequency(combined_ts))
+  return(ret)
+}
+
+#' Subset time series with provided indices
+#'
+#' Use provided indices to subset a time series. The provided indices must be contiguous
+#' 
+#' @export
+#' @param x A time series object
+#' @param indices A contiguous vector of indices to use for subsetting
+#' @return A time series object appropiately subsetted using provided indices
+#' 
+#' @author Ganesh Krishnan 
+#' @examples 
+#' tsSubsetWithIndices(AirPassengers, c(3:10))
+tsSubsetWithIndices <- function(x, indices) {
+  xtime <- time(x)
+  minIndex <- min(indices)
+  maxIndex <- max(indices)
+
+  if (maxIndex > length(xtime)) stop("Max subset index cannot exceed time series length")
+  if (all(seq(minIndex, maxIndex, 1) != indices)) stop("Time series can only be subset with continuous indices")
+  return(window(x, start = xtime[minIndex], end = xtime[maxIndex]))
 }
